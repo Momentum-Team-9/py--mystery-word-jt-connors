e1c20d669be822366272d2c1cf0bc193f366d191
“Joseph <jtconnors94@gmail.com>
2021-08-08 16:37:21 -0400

    working loop to choose a word for a specific difficulty with help from em

--- mystery_word.py
+++ mystery_word.py
@@ -1,26 +1,51 @@
 import random
 
+
 def mystery_word(file):
     file = open(file)
-    text = file.read()
-    text = text.split()
-    random_word = random.choice(text)
+    words = file.readlines()
+    file.close()
+    
+    #letter_guesses = []
+    #number_of_guesses = 0
 
-    #easy = random_word(len <= 6)
-
-    letter_guesses = []
-    number_of_guesses = 0
-
-    user_input_level = input(' Choose your difficulty: | (E)asy | (M)edium | (H)ard')
-    user_input_guess = input('Guess a letter in your mystery word' )
+    input_level = input(' Choose your Mode: | (E)asy | (M)edium | (H)ard ')
+    #input_guess = input(' Guess a letter in your mystery word ')
 
     #Easy
     # if user chooses Easy user will get a length of 4 or less
-    if user_input_level == "E":
-        print(random_word)
-        for letter in random_word:
-            if user_input_guess in random_word:
-                while number_of_guesses < 8:
+    while input_level != "quit":
+        mystery_word_list = []
+        if input_level == "E":
+            for word in words:
+                if (len(word) >= 4 and len(word) <= 6):
+                    mystery_word_list.append(word)
+                    random_word = random.choice(mystery_word_list)
+            break
+        elif input_level == "M":
+            for word in words:
+                if (len(word) >= 6 and len(word) <= 8):
+                    mystery_word_list.append(word)
+                    random_word = random.choice(mystery_word_list)
+            break
+        elif input_level == "H":
+            for word in words:
+                if len(word) > 8:
+                    mystery_word_list.append(word)
+                    random_word = random.choice(mystery_word_list)
+            break
+    print(random_word)
+
+    #elif input_level == "M":
+        #mystery_word = random.choice(medium_words)
+        #print(mystery_word)
+    #elif input_level == "H":
+        #mystery_word = random.choice(hard_words)
+        #print(mystery_word)
+                #print(f"the mystery word has {len(random_word)} letters ")
+                #for letter in random_word:
+                   # if input_guess in random_word:
+                      #  while number_of_guesses < 8:
                     
         
     #Medium
@@ -29,22 +54,9 @@ def mystery_word(file):
     #Hard
     # if user chooses Hard user will get a length of 8 or more
     #print(text)
-                    file.close()
+        
 
-    #python3 mystery_word.py words.txt
+#python3 mystery_word.py words.txt
 
-if __name__ == "__main__":
-    import argparse
-    from pathlib import Path
 
-    parser = argparse.ArgumentParser(
-        description='Get the mystery_word in a text file.')
-    parser.add_argument('file', help='file to read')
-    args = parser.parse_args()
-
-    file = Path(args.file)
-    if file.is_file():
-        mystery_word(file)
-    else:
-        print(f"{file} does not exist!")
-        exit(1)
+mystery_word("words.txt")
