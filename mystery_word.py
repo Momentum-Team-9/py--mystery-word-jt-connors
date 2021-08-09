ec39f2c5612aceb75746864d3d86da7c7598fce4
“Joseph <jtconnors94@gmail.com>
2021-08-08 20:59:02 -0400

    attempt at adding guess number tries

--- mystery_word.py
+++ mystery_word.py
@@ -5,57 +5,48 @@ def mystery_word(file):
     file = open(file)
     words = file.readlines()
     file.close()
-    
-    #letter_guesses = []
-    #number_of_guesses = 0
 
     input_level = input(' Choose your Mode: | (E)asy | (M)edium | (H)ard ')
-    #input_guess = input(' Guess a letter in your mystery word ')
 
     #Easy
     # if user chooses Easy user will get a length of 4 or less
     while input_level != "quit":
         mystery_word_list = []
-        if input_level == "E":
+        if input_level == "E" or "e":
             for word in words:
                 if (len(word) >= 4 and len(word) <= 6):
                     mystery_word_list.append(word)
                     random_word = random.choice(mystery_word_list)
             break
-        elif input_level == "M":
+        elif input_level == "M" or "m":
             for word in words:
                 if (len(word) >= 6 and len(word) <= 8):
                     mystery_word_list.append(word)
                     random_word = random.choice(mystery_word_list)
             break
-        elif input_level == "H":
+        elif input_level == "H" or "h":
             for word in words:
-                if len(word) > 8:
+                if len(word) < 8:
                     mystery_word_list.append(word)
                     random_word = random.choice(mystery_word_list)
             break
     print(random_word)
-
-    #elif input_level == "M":
-        #mystery_word = random.choice(medium_words)
-        #print(mystery_word)
-    #elif input_level == "H":
-        #mystery_word = random.choice(hard_words)
-        #print(mystery_word)
-                #print(f"the mystery word has {len(random_word)} letters ")
-                #for letter in random_word:
-                   # if input_guess in random_word:
-                      #  while number_of_guesses < 8:
-                    
         
-    #Medium
-    #if user chooses Medium user will get a length of 6-8c
+    guess_number = 0
+    guess_letter = []
 
-    #Hard
-    # if user chooses Hard user will get a length of 8 or more
-    #print(text)
+    input_guess = input(' Guess a letter in your mystery word ')
+    while guess_number < 8:
+        if input_guess in random_word:
+            guess_letter.append(input_guess)
+            print(guess_letter)
+        else:
+            guess_number += 1
+            print(guess_number)
+            return("_")
         
-
+    if guess_number == 8:
+        print("game over")
 #python3 mystery_word.py words.txt
 
 
